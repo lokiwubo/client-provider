8d42532b8a83c3fd0387c99274c96ba84b17fcc0
wubo <wb31202239@163.com>
2024-12-18 16:51:21 +0800

    fix:修改bug object assgin顺序导致的问题

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import { HttpClient } from './core';
 import { DefineHttpClient, HttpClientMiddleware, RequestTemplate } from './types';
 
 export const definedCreateHttpClient: DefineHttpClient = (context) => {
-  const middlewaresSet = new Set<HttpClientMiddleware>([]);
-  const httpClient = new HttpClient(() => [...middlewaresSet]);
-  return Object.assign({
-    use: (middleware: HttpClientMiddleware) => {
-      middlewaresSet.add(middleware);
-      return () => {
-        middlewaresSet.delete(middleware);
-      };
-    },
-    createClient: <T extends RequestTemplate>(
-      defineApis: (apis: HttpClient, context: AnyLike) => T,
-    ) => {
-      return defineApis(httpClient, context);
-    },
-  });
+    const middlewaresSet = new Set<HttpClientMiddleware>([]);
+    const httpClient = new HttpClient(() => [...middlewaresSet]);
+    return Object.assign(
+        <T extends RequestTemplate>(defineApis: (apis: HttpClient, context: AnyLike) => T) => {
+            return defineApis(httpClient, context);
+        },
+        {
+            use: (middleware: HttpClientMiddleware) => {
+                middlewaresSet.add(middleware);
+                return () => {
+                    middlewaresSet.delete(middleware);
+                };
+            },
+        },
+    );
 };
